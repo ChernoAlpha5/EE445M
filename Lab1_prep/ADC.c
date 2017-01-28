4d921fb3129d95d56a8335f6c060fb79846f2552
Andrew W <andrwww@utexas.edu>
2017-01-28 02:09:05 -0600

    Added ADC stuff for real this time. Needs some testing.

---
+++ Lab1_prep/ADC.c
@@ -0,0 +1,216 @@
+// ADCSWTrigger.c
+// Runs on TM4C123
+// Provide functions that initialize ADC0 SS3 to be triggered by
+// software and trigger a conversion, wait for it to finish,
+// and return the result.
+// Daniel Valvano
+// August 6, 2015
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+#include "ADC.h"
+#include "PLL.h"
+
+// There are many choices to make when using the ADC, and many
+// different combinations of settings will all do basically the
+// same thing.  For simplicity, this function makes some choices
+// for you.  When calling this function, be sure that it does
+// not conflict with any other software that may be running on
+// the microcontroller.  Particularly, ADC0 sample sequencer 3
+// is used here because it only takes one sample, and only one
+// sample is absolutely needed.  Sample sequencer 3 generates a
+// raw interrupt when the conversion is complete, but it is not
+// promoted to a controller interrupt.  Software triggers the
+// ADC0 conversion and waits for the conversion to finish.  If
+// somewhat precise periodic measurements are required, the
+// software trigger can occur in a periodic interrupt.  This
+// approach has the advantage of being simple.  However, it does
+// not guarantee real-time.
+//
+// A better approach would be to use a hardware timer to trigger
+// the ADC0 conversion independently from software and generate
+// an interrupt when the conversion is finished.  Then, the
+// software can transfer the conversion result to memory and
+// process it after all measurements are complete.
+
+// This initialization function sets up the ADC according to the
+// following parameters.  Any parameters not explicitly listed
+// below are not modified:
+// Max sample rate: <=125,000 samples/second
+// Sequencer 0 priority: 1st (highest)
+// Sequencer 1 priority: 2nd
+// Sequencer 2 priority: 3rd
+// Sequencer 3 priority: 4th (lowest)
+// SS3 triggering event: software trigger
+// SS3 1st sample source: Ain9 (PE4)
+// SS3 interrupts: enabled but not promoted to controller
+
+#define PF2             (*((volatile uint32_t *)0x40025010))
+#define PF1             (*((volatile uint32_t *)0x40025008))
+
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+
+
+volatile uint32_t ADCvalue;
+int numSamples = 0;
+int counter = 0;
+int collectFinished = 0;
+int* buffer;
+	
+void ADC0_InitSWTriggerSeq3(uint32_t channelNum){ 
+  SYSCTL_RCGCADC_R |= 0x0001;   // 7) activate ADC0 
+                                  // 1) activate clock for Port E
+  SYSCTL_RCGCGPIO_R |= 0x10;
+  while((SYSCTL_PRGPIO_R&0x10) != 0x10){};
+  GPIO_PORTE_DIR_R &= ~0x10;      // 2) make PE4 input
+  GPIO_PORTE_AFSEL_R |= 0x10;     // 3) enable alternate function on PE4
+  GPIO_PORTE_DEN_R &= ~0x10;      // 4) disable digital I/O on PE4
+  GPIO_PORTE_AMSEL_R |= 0x10;     // 5) enable analog functionality on PE4
+    
+//  while((SYSCTL_PRADC_R&0x0001) != 0x0001){};    // good code, but not yet implemented in simulator
+
+
+  ADC0_PC_R &= ~0xF;              // 7) clear max sample rate field
+  ADC0_PC_R |= 0x1;               //    configure for 125K samples/sec
+  ADC0_SSPRI_R = 0x0123;          // 8) Sequencer 3 is highest priority
+  ADC0_ACTSS_R &= ~0x0008;        // 9) disable sample sequencer 3
+  ADC0_EMUX_R &= ~0xF000;         // 10) seq3 is software trigger
+  ADC0_SSMUX3_R &= ~0x000F;       // 11) clear SS3 field
+  ADC0_SSMUX3_R += channelNum;             //    set channel
+  ADC0_SSCTL3_R = 0x0006;         // 12) no TS0 D0, yes IE0 END0
+  ADC0_IM_R &= ~0x0008;           // 13) disable SS3 interrupts
+  ADC0_ACTSS_R |= 0x0008;         // 14) enable sample sequencer 3
+}
+
+
+//------------ADC_In------------
+// Busy-wait Analog to digital conversion
+// Input: none
+// Output: 12-bit result of ADC conversion
+int ADC_In(void){  
+	int result;
+  ADC0_PSSI_R = 0x0008;            // 1) initiate SS3
+  while((ADC0_RIS_R&0x08)==0){};   // 2) wait for conversion done
+    // if you have an A0-A3 revision number, you need to add an 8 usec wait here
+  result = ADC0_SSFIFO3_R&0xFFF;   // 3) read result
+  ADC0_ISC_R = 0x0008;             // 4) acknowledge completion
+  return result;
+}
+
+// ***************** Timer2_Init ****************
+// Activate Timer2 interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in units (1/clockfreq)
+// Outputs: none
+void Timer2_Init(/*void(*task)(void),*/ unsigned long period){
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  //PeriodicTask = task;          // user function
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+}
+
+/*void Timer0A_Init100HzInt(void){
+  volatile uint32_t delay;
+  DisableInterrupts();
+  // **** general initialization ****
+  SYSCTL_RCGCTIMER_R |= 0x01;      // activate timer0
+  delay = SYSCTL_RCGCTIMER_R;      // allow time to finish activating
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = 0;                // configure for 32-bit timer mode
+  // **** timer0A initialization ****
+                                   // configure for periodic mode
+  TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER0_TAILR_R = 799999;         // start value for 100 Hz interrupts
+  TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
+  // **** interrupt initialization ****
+                                   // Timer0A=priority 2
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
+  NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+}*/
+void Timer2A_Handler(void){
+  TIMER2_ICR_R = TIMER_ICR_TATOCINT;    // acknowledge timer2A timeout
+  PF2 ^= 0x04;         	// profile
+	if (counter < numSamples){
+		*(buffer + counter) = ADC_In();
+	  counter++;
+	}
+	else{
+		counter = 0;
+		numSamples = 0;
+		collectFinished = 0;				// finished sampling
+		TIMER2_CTL_R = 0x00000000;		// disable timer2
+	}
+  //PF2 ^= 0x04;                   // profile
+  PF2 ^= 0x04;                   // profile
+}
+
+// ***************** ADC_Open ****************
+// Initialize ADC on specified channel
+// Inputs:  ADC channel to initialize
+// Outputs: none
+int ADC_Open(uint32_t channelNum){
+	
+	SYSCTL_RCGCGPIO_R |= 0x20;            // activate port F
+  ADC0_InitSWTriggerSeq3(channelNum);         // allow time to finish activating
+  GPIO_PORTF_DIR_R |= 0x06;             // make PF2, PF1 out (built-in LED)
+  GPIO_PORTF_AFSEL_R &= ~0x06;          // disable alt funct on PF2, PF1
+  GPIO_PORTF_DEN_R |= 0x06;             // enable digital I/O on PF2, PF1
+                                        // configure PF2 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF00F)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;               // disable analog functionality on PF
+  PF2 = 0;                      // turn off LED
+	return 0;
+}
+
+// ***************** ADC_Open ****************
+// CollectADC on specified channel
+// Inputs:  
+// Outputs: none
+ void ADC_Collect(int channel_num, int sampling_rate, int* data_buffer, int num_samples){
+	collectFinished = 1;
+	ADC_Open(channel_num);
+	Timer2_Init(sampling_rate);               	// set up Timer2A for interrupts, period specified by sampling_rate. FIX THIS!!!
+	buffer = data_buffer;
+	numSamples = num_samples;
+	EnableInterrupts();
+}
+ 
+// ***************** ADC_Status ****************
+// Return status of ADC_Collect
+// Inputs:  none
+// Outputs: 0 if ADC_Collect done, 1 if still waiting
+int ADC_Status(){
+	return collectFinished;
+}
+
+
+
