645c074b535376c9e8a4cb2a474bb9e1edcba9c1
Andrew W <andrwww@utexas.edu>
2017-02-26 19:25:22 -0600

    Lab 3 initial commit

---
+++ Lab3/ADC.h
@@ -0,0 +1,29 @@
+// ADCT0ATrigger.h
+// Runs on LM4F120/TM4C123
+// Provide a function that initializes Timer0A to trigger ADC
+// SS3 conversions and request an interrupt when the conversion
+// is complete.
+// Daniel Valvano
+// May 2, 2015
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+int ADC_Open(uint32_t channelNum);
+
+uint16_t ADC_In(void);
+
+int ADC_Collect(uint32_t channelNum, uint32_t fs, void (*task)(unsigned long));
---
+++ Lab3/os.d
@@ -0,0 +1,8 @@
+.\os.o: OS.c
+.\os.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\os.o: ../inc/tm4c123gh6pm.h
+.\os.o: UART.H
+.\os.o: OS.h
+.\os.o: SysTick.h
+.\os.o: PLL.h
+.\os.o: ST7735.h
